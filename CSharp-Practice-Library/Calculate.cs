1b232ab629a5bc63f9c26ef131b1ea9809adf3dc
Josh Bentele <jbentele@outlook.com>
2020-07-25 18:30:13 -0500

    Add project files.

---
+++ CSharp-Practice-Library/Calculate.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_Practice_Library
+{
+    public class Calculate
+    {
+        public static void Calculator()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Welcome to a Crappy Calculator!");
+            bool calculateLoop = false;
+            do
+            {
+                Console.Write("Enter the first number: ");
+                string input1 = Console.ReadLine();
+                int num1;
+                bool convert1 = Int32.TryParse(input1, out num1);
+                if (convert1)
+                {
+                    Console.Write("Enter the operand you would like to use (+-*/): ");
+                    string operation = Console.ReadLine();
+                    if (operation == "+" || operation == "-" || operation == "*" || operation == "/")
+                    {
+                        Console.Write("Enter the second number: ");
+                        string input2 = Console.ReadLine();
+                        int num2;
+                        bool convert2 = Int32.TryParse(input2, out num2);
+                        if (convert2)
+                        {
+                            int sum;
+                            switch (operation)
+                            {
+                                case "+":
+                                    sum = num1 + num2;
+                                    Console.WriteLine("Result:\n{0} + {1} = {2}", num1, num2, sum);
+                                    calculateLoop = true;
+                                    break;
+                                case "-":
+                                    sum = num1 - num2;
+                                    Console.WriteLine("Result:\n{0} - {1} = {2}", num1, num2, sum);
+                                    calculateLoop = true;
+                                    break;
+                                case "*":
+                                    sum = num1 * num2;
+                                    Console.WriteLine("Result:\n{0} * {1} = {2}", num1, num2, sum);
+                                    calculateLoop = true;
+                                    break;
+                                case "/":
+                                    sum = num1 / num2;
+                                    Console.WriteLine("Result:\n{0} / {1} = {2}", num1, num2, sum);
+                                    calculateLoop = true;
+                                    break;
+                                default:
+                                    Console.WriteLine("Something went wrong...");
+                                    calculateLoop = true;
+                                    break;
+                            }
+
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Please enter numerical values only.\nYou entered: " + input2);
+                            calculateLoop = false;
+                        }
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Please enter a valid operator! (+-*/).\nYou entered: " + operation);
+                        calculateLoop = false;
+                    }
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please enter numerical values only.\nYou entered: " + input1);
+                    calculateLoop = false;
+                }
+            } while (!calculateLoop);
+        }
+    }
+}
---
+++ CSharp-Practice-Library/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace CSharp_Practice_Library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainMenu();
+        }
+
+        private static void MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to Josh's C# Practice Library!\nFeel free to look around and check out one of the exercises below!");
+            GetMenuOptions();
+            Console.Write("Select an exercise? ");
+            NavigateRequest(Console.ReadLine());
+        }
+
+        private static void GetMenuOptions()
+        {
+            string[] options = { "1) Crappy Calculator" };
+
+            foreach (string option in options)
+            {
+                Console.WriteLine(option);
+            }
+        }
+
+        private static void NavigateRequest(string userRequest)
+        {
+            int selection;
+            bool validRequest = Int32.TryParse(userRequest, out selection);
+            if (validRequest)
+            {
+                switch (selection)
+                {
+                    case 1:
+                        Calculate.Calculator();
+                        break;
+                    default:
+                        MainMenu();
+                        break;
+                }
+            }
+            else
+            {
+                MainMenu();
+            }
+        }
+    }
+}
